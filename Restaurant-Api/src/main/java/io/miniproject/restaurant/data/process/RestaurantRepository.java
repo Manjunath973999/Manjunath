b01fc7aff5bc161782de1d131de7a469f48ebb99
Manjunath973999 <manju831chandu@gmail.com>
2020-06-26 21:00:36 +0530

    Update

--- Restaurant-Api/src/main/java/io/miniproject/restaurant/data/process/RestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package io.miniproject.restaurant.data.process;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
-
-}
